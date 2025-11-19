5fe5934950c78a94ca38ab327d2c01db55584e3a
kieran hervold <kieran.hervold@elegenbio.com>
2025-11-19 10:51:54 -0800

    more tests

--- tests/test_comp.py
+++ tests/test_comp.py
@@ -1,10 +1,22 @@
 from sigfigfloat import SigFigFloat
 
+FULL_PREC = 1.4999999999
+SIG_FIG = SigFigFloat(FULL_PREC)
 
 def test_basic():
-    full_prec = 1.4999999999
-    sig_fig = SigFigFloat(full_prec)
+    assert SIG_FIG.orig_val == FULL_PREC
+    assert SIG_FIG.rounded == 1.5
 
-    assert sig_fig == sig_fig
-    assert sig_fig == full_prec
-    assert sig_fig > 1.49
+    assert SIG_FIG == SIG_FIG
+    assert SIG_FIG == FULL_PREC
+    assert SIG_FIG > 1.49
+
+def test_str():
+    assert str(SIG_FIG) == "1.50"
+    assert repr(SIG_FIG) == "1.4999999999"
+
+def test_coercion():
+    orig_val = float(SIG_FIG)
+    assert isinstance(orig_val, float)
+    assert orig_val == FULL_PREC
+    assert str(orig_val) != str(SIG_FIG)
